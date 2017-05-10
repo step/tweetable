ebbfc7e63eb741d9bce42f681c674e0bb2dea5f1
surajit-chongder <surajit.chongder@gmail.com>
2017-05-10 17:14:31 +0530

    [Surajit/Vathsala] Added seed data for the user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([{name: 'Kamal Hasan', admin:false, auth_id: '132271', image_url: 'http://graph.facebook.com/demo1'},
+              {name: 'Rajanikanth', admin:true, auth_id: '132272', image_url: 'http://graph.facebook.com/demo2'}])
