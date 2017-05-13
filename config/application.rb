cf83a3fb51f3a2646cb02abb7fd145dc0b139235
dharmenn <dharmenn@thoughtworks.com>
2017-05-13 12:48:55 +0530

    [Dharmenn] miscellaneous :
     - Updating rails generator config so that it does not generate js and css

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Tweetable
 
     config.generators do |g|
       g.template_engine :erb
+      g.stylesheets     false
+      g.javascripts     false
       g.test_framework  :rspec, :fixture => true, :views => false
       g.integration_tool :rspec, :fixture => true, :views => true
       g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
