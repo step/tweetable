19988e9454f145810287643ca9b2467f1729811c
dharmenn <dharmenn@thoughtworks.com>
2017-05-22 23:00:31 +0530

    [Dharmenn] [tweetable/#18] Tweets :
     - Adding validation for response model
     - Adding test for existing associations

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,8 +1,13 @@
 class Response < ApplicationRecord
+  validates :text, presence: true
+  validates_length_of :text, maximum:140, on: :create ,too_long: "Tweet length can't be more than 140 characters"
+  validates :user_id, presence: true
+  validates :passage_id, presence: true
+
   belongs_to :passage
   belongs_to :user
-  has_many :tagging
-  has_many :tag, through: :tagging
+  has_many :taggings
+  has_many :tags, through: :taggings
   accepts_nested_attributes_for :user, :passage
 
 end
---
+++ spec/models/response_spec.rb
@@ -0,0 +1,20 @@
+describe Response do
+
+  describe 'validations ' do
+    it {should validate_presence_of(:text)}
+
+    it {should validate_presence_of(:user_id)}
+
+    it {should validate_presence_of(:passage_id)}
+    it {should validate_length_of(:text).is_at_most(140).on(:create).with_long_message("Tweet length can't be more than 140 characters")}
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:passage) }
+    it { should have_many(:taggings) }
+    it { should have_many(:tags).through(:taggings) }
+
+  end
+
+end
