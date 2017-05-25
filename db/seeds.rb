50475813e5b5e0f47a4fb521f2d0fb0d6ea90415
surajit-chongder <surajit.chongder@gmail.com>
2017-05-25 17:51:32 +0530

    [Surajit/Sooraj] Changed seed data and ignore past date validation for passage in a seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
-User.create!(
+# --------------- User Seeds ---------------------
+User.create(
     [
         {
             name: 'Kamal Hasan', admin: false, auth_id: '132271', image_url: 'http://graph.facebook.com/demo1'
@@ -12,7 +13,9 @@ User.create!(
     ]
 )
 
-Passage.create!(
+# --------------- Passage Seeds ---------------------
+
+Passage.create(
     [
         {
             title: 'Climate Change', text: 'climate change passage', start_time: DateTime.now, close_time: (DateTime.now+2), duration: '3600'
@@ -21,15 +24,8 @@ Passage.create!(
         {
             title: 'Person', text: 'person passage', start_time: DateTime.now, close_time: (DateTime.now+1), duration: '7200'
         },
-
-        {
-            title: 'News', text: 'news passage', start_time: (DateTime.now-2), close_time: (DateTime.now-1), duration: '7200'
-        },
         {
             title: 'Program', text: 'program passage', start_time: (DateTime.now-3), close_time: (DateTime.now+1), duration: '7200'
-        },
-        {
-            title: 'Class', text: 'class passage', start_time: (DateTime.now-3), close_time: (DateTime.now-1), duration: '7200'
         },
         {
             title: 'Computer', text: 'computer passage', start_time: (DateTime.now+3), close_time: (DateTime.now+7), duration: '7200'
@@ -40,7 +36,13 @@ Passage.create!(
     ]
 )
 
-Response.create!(
+Passage.new(title: 'News', text: 'news passage', start_time: (DateTime.now-2), close_time: (DateTime.now-1), duration: '7200').save(validate: false)
+Passage.new(title: 'Class', text: 'class passage', start_time: (DateTime.now-3), close_time: (DateTime.now-1), duration: '7200').save(validate: false)
+
+
+# --------------- Response Seeds ---------------------
+
+Response.create(
     [
       {
           text: "respose for Climate Changed", user_id: User.find_by(auth_id: '132271').id, passage_id: Passage.find_by(title: 'Climate Change').id
@@ -60,7 +62,9 @@ Response.create!(
   ]
 )
 
-Tag.create!(
+# --------------- Tag Seeds ---------------------
+
+Tag.create(
     [
         {
             name: 'Grammatical Error', weight: -5, description: 'something'
@@ -74,7 +78,9 @@ Tag.create!(
     ]
 )
 
-Tagging.create!(
+# --------------- Tagging Seeds ---------------------
+
+Tagging.create(
     [
         {
             response_id: Response.find_by(text: 'respose for Climate Changed').id, tag_id: Tag.find_by(name: 'Grammatical Error').id
