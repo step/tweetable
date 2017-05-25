80c4ff965ad23771fa97c6fa5afafe26d7f782a5
surajit-chongder <surajit.chongder@gmail.com>
2017-05-25 14:12:26 +0530

    [Surajit] Removed unnecessary stub data from passage spec

--- spec/controllers/passages_controller_spec.rb
+++ spec/controllers/passages_controller_spec.rb
@@ -15,61 +15,14 @@ RSpec.describe PassagesController, type: :controller do
     }
   }
 
-  let(:users) {
-
-    [
-        {
-            name: 'Kamal Hasan', admin: false, auth_id: '132271', image_url: 'http://graph.facebook.com/demo1'
-        },
-        {
-            name: 'Vimal Hasan', admin: false, auth_id: '132273', image_url: 'http://graph.facebook.com/demo1'
-        },
-        {
-            name: 'Rajanikanth', admin: false, auth_id: '132272', image_url: 'http://graph.facebook.com/demo2'
-        }
-    ]
-  }
-
   let(:passages) {
     [
         {
             title: 'Climate Change', text: 'climate change passage', start_time: DateTime.now, close_time: (DateTime.now+2), duration: '1'
-        },
-
-        {
-            title: 'Person', text: 'person passage', start_time: DateTime.now, close_time: (DateTime.now+1), duration: '2'
-        },
-
-        {
-            title: 'News', text: 'news passage', start_time: (DateTime.now-2), close_time: (DateTime.now-1), duration: '2'
-        },
-        {
-            title: 'Program', text: 'program passage', start_time: (DateTime.now-3), close_time: (DateTime.now+1), duration: '2'
-        },
-        {
-            title: 'Class', text: 'class passage', start_time: (DateTime.now-3), close_time: (DateTime.now-1), duration: '2'
         }
     ]
+  }
 
-  }
-  let(:responses) { [
-      {
-          text: "respose for Climate Changed", user_id: User.find_by(auth_id: '132271').id, passage_id: Passage.find_by(title: 'Climate Change').id
-      },
-      {
-          text: "respose for Climate Changed", user_id: User.find_by(auth_id: '132273').id, passage_id: Passage.find_by(title: 'Climate Change').id
-      },
-      {
-          text: "respose for Person", user_id: User.find_by(auth_id: '132271').id, passage_id: Passage.find_by(title: 'Person').id
-      },
-      {
-          text: "respose for Person", user_id: User.find_by(auth_id: '132273').id, passage_id: Passage.find_by(title: 'Person').id
-      },
-      {
-          text: "News Response", user_id: User.find_by(auth_id: '132273').id, passage_id: Passage.find_by(title: 'News').id
-      }
-  ]
-  }
   before(:each) do
     stub_logged_in(true)
   end
@@ -94,6 +47,7 @@ RSpec.describe PassagesController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
+
     before(:each) do
       @passages = Passage.create!(passages)
     end
@@ -114,13 +68,6 @@ RSpec.describe PassagesController, type: :controller do
   end
 
   describe 'filter methods' do
-    before(:each) do
-      @passages = Passage.create!(passages)
-    end
-
-    after(:each) do
-      @passages.each(&:delete)
-    end
 
     describe 'GET #drafts' do
       context 'Admin' do
@@ -204,7 +151,7 @@ RSpec.describe PassagesController, type: :controller do
         end
         context 'When the request is generated withing the tabs' do
           it 'should give all the yet to closed passages' do
-            get :closed, params:{from_tab: true}
+            get :closed, params: {from_tab: true}
 
             expect(flash[:danger]).to be_nil
             expect(response).to be_success
@@ -221,6 +168,7 @@ RSpec.describe PassagesController, type: :controller do
           end
         end
       end
+
       context 'Candidate' do
         before(:each) do
           user = double('User', admin: false)
@@ -237,7 +185,7 @@ RSpec.describe PassagesController, type: :controller do
     describe "GET #open_for_candidate" do
       it 'should give all the yet to open passages' do
         stub_current_user(double('User', passages: []))
-        get :open_for_candidate, params: {from_tab:true}
+        get :open_for_candidate, params: {from_tab: true}
         expect(response).to be_success
         should render_template('passages/candidate/passages_pane',)
       end
