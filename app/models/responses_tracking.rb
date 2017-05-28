17796d46c087219c591ceae0306fd8d8d681cfee
Vathsala-M <vathsala@thoughtworks.com>
2017-05-28 13:19:06 +0530

    [Vathsala/Abhishek] [tweetable/#30] Sync remaining time with backend : - Delete start_time, end_time and duration columns from responses tracking model - Add created_at and updated_at columns with timestamps

--- app/models/responses_tracking.rb
+++ app/models/responses_tracking.rb
@@ -1,28 +1,20 @@
 class ResponsesTracking < ApplicationRecord
   validates :user_id, presence: true
   validates :passage_id, presence: true
-  validates :start_time, presence: true
 
   belongs_to :passage
   belongs_to :user
 
   def self.remaining_time(passage_id, user_id)
-    tracking_detail = ResponsesTracking.find_by({passage_id: passage_id, user_id: user_id})
-    if tracking_detail.nil?
-      tracking_detail = create_tracking_details(passage_id, user_id)
-    end
+    tracking_detail = ResponsesTracking.find_or_create_by({passage_id: passage_id, user_id: user_id})
     close_time_duration = tracking_detail.passage.close_time - DateTime.now.utc
     passage_duration = tracking_detail.passage.duration
-    remaining_time = passage_duration - (DateTime.now.utc - tracking_detail.start_time)
+    remaining_time = passage_duration - (DateTime.now.utc - tracking_detail.created_at)
     [remaining_time,close_time_duration].min
-  end
-
-  def self.create_tracking_details(passage_id, user_id)
-    ResponsesTracking.create({passage_id: passage_id, user_id: user_id, start_time: DateTime.now})
   end
 
   def self.update_end_time(passage_id, user_id)
     tracking_detail = ResponsesTracking.find_by({passage_id: passage_id, user_id: user_id})
-    tracking_detail.update_attributes(end_time:DateTime.now)
+    tracking_detail.touch(:updated_at)
   end
 end
--- db/migrate/20170525100040_create_responses_tracking.rb
+++ db/migrate/20170525100040_create_responses_tracking.rb
@@ -3,9 +3,7 @@ class CreateResponsesTracking < ActiveRecord::Migration[5.1]
     create_table :responses_trackings do |t|
       t.references :user, foreign_key: true
       t.references :passage, foreign_key: true
-      t.datetime :start_time
-      t.datetime :end_time
-      t.integer :duration
+      t.timestamps
     end
   end
 end
