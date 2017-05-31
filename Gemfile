6ae6b655dbf00908a7ebdeb98ebb353d010cec22
Abhishek Gupta <abhishkg@thoughtworks.com>
2017-05-31 20:34:24 +0530

    [Abhishek] Using Active Record Session Store  For better security practise we are using Active Record Session Store rather than Simple Cookie Store

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jbuilder', '~> 2.5'
 
 gem 'bootstrap_form'
 gem 'jquery-rails'
+gem 'activerecord-session_store' , '~> 1.1.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170531044446) do
+ActiveRecord::Schema.define(version: 20170531145213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,15 @@ ActiveRecord::Schema.define(version: 20170531044446) do
     t.datetime "updated_at", null: false
     t.index ["passage_id"], name: "index_responses_trackings_on_passage_id"
     t.index ["user_id"], name: "index_responses_trackings_on_user_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "taggings", force: :cascade do |t|
