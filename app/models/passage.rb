4964f277a20ff8ae76c20ef4794726e63e2eb4ca
Sooraj Parameswaran <soorajp@thoughtworks.com>
2017-06-01 13:54:03 +0530

    [BugFix] [Sooraj] Fixing passage validations which was blocking conclude feature

--- app/models/passage.rb
+++ app/models/passage.rb
@@ -12,12 +12,11 @@ class Passage < ApplicationRecord
   DEFAULT_DURATION = 3600
 
   def commence(conclude_time)
-    self.update_attributes(commence_time: Time.current,conclude_time: ( conclude_time))
+    update_attributes(commence_time: Time.current, conclude_time: (conclude_time))
   end
 
   def conclude
-    self.conclude_time = Time.current
-    self.save
+    update_attributes(conclude_time:Time.current)
   end
 
   def self.drafts
@@ -39,8 +38,8 @@ class Passage < ApplicationRecord
   end
 
   def self.missed_by_candidate(user)
-    missed = self.get_timed_out_passages(self.ongoing,user.id)
-    (self.finished + missed) - user.passages
+    timed_out_passages = self.get_timed_out_passages(self.ongoing,user.id)
+    (self.finished + timed_out_passages) - user.passages
   end
 
   def self.attempted_by_candidate(user)
@@ -50,39 +49,38 @@ class Passage < ApplicationRecord
   end
 
   private
+
   def self.get_passages_with_corresponding_response(responses, passage_id)
     responses.find {|response| response.passage_id.equal?(passage_id)}
   end
 
-  def self.is_passage_missed(passage,user_id)
-    tracking_details = ResponsesTracking.find_by({passage_id:passage.id,user_id:user_id})
+  def self.is_passage_missed(passage, user_id)
+    tracking_details = ResponsesTracking.find_by({passage_id: passage.id, user_id: user_id})
     unless tracking_details.nil?
-      ResponsesTracking.remaining_time(passage.id,user_id) <= 0
+      ResponsesTracking.remaining_time(passage.id, user_id) <= 0
     end
   end
 
-  def self.get_timed_out_passages(ongoing_passages,user_id)
-    ongoing_passages.select {|passage| is_passage_missed(passage,user_id)}
+  def self.get_timed_out_passages(ongoing_passages, user_id)
+    ongoing_passages.select {|passage| is_passage_missed(passage, user_id)}
   end
 
-
   def is_valid_conclude_time?
-    if (self.conclude_time.present?  and invalid_conclude_time?) or is_valid_commence_time?
+    unless (self.conclude_time.present? and valid_conclude_time?) and is_valid_commence_time?
       errors.add(:conclude_time, 'must be a future time...')
     end
   end
 
   def is_valid_commence_time?
-    self.commence_time.present?  and invalid_commence_time?
+    self.commence_time.present? and valid_commence_time?
   end
 
-  def invalid_conclude_time?
-    self.conclude_time <= Time.current
+  def valid_conclude_time?
+    self.conclude_time >= Time.current
   end
 
-
-  def invalid_commence_time?
-    self.conclude_time <= self.commence_time
+  def valid_commence_time?
+    self.conclude_time > self.commence_time
   end
 
   def defaults
--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -31,30 +31,31 @@ describe ResponsesController, type: :controller do
   end
 
   describe 'Post #create' do
+    let(:passage) {double('passage', id: 'passage_id', title: 'Test passage title', text: 'Test passage text', duration: 1)}
+    let(:intern) {double('intern', id: 'user_id', name: 'Intern', auth_id: 'auth_id', admin: false)}
 
-    let(:passage) {Passage.create(title: 'Test passage title', text: 'Test passage text', duration: 1)}
-    let(:intern) {User.create(name: 'Intern', auth_id: '1243234324')}
+    before(:each) do
+      stub_logged_in(true)
+      stub_current_user(intern)
+    end
 
     context 'When user and passage exists' do
       context 'When Responding time is over' do
         context 'Response length is valid' do
           it 'should create  response' do
-            stub_logged_in(true)
-            user = double('User', admin: false, id: 1)
-            stub_current_user(user)
             expect(ResponsesTracking).to receive(:remaining_time).and_return(0)
+            expect(Passage).to receive(:find).and_return(passage)
 
             post :create, params: {passage_id: passage.id, user_id: intern.id, text: 'Response to test passage'}
             expect(response).to redirect_to(passages_path)
             expect(flash[:danger]).to match('Sorry, Your response submission time is expired!')
           end
         end
+
         context 'Response length is invalid' do
           it 'should not create  response' do
-            stub_logged_in(true)
-            user = double('User', admin: false, id: 1)
-            stub_current_user(user)
             expect(ResponsesTracking).to receive(:remaining_time).and_return(-1)
+            expect(Passage).to receive(:find).and_return(passage)
 
             post :create, params: {passage_id: passage.id, user_id: intern.id, text: 'This is an invalid response because its contains more that 140 characters. This is an invalid response because its contains more that 140 chars.'}
             expect(response).to redirect_to(passages_path)
@@ -62,26 +63,31 @@ describe ResponsesController, type: :controller do
           end
         end
       end
+
       context 'When Responding time is not over' do
+        let(:response) {double('response')}
+
         context 'Response length is valid' do
           it 'should create  response' do
-            stub_logged_in(true)
-            user = double('User', admin: false, id: 1)
-            stub_current_user(user)
             expect(ResponsesTracking).to receive(:update_end_time)
             expect(ResponsesTracking).to receive(:remaining_time).and_return(2)
+            expect(Passage).to receive(:find).and_return(passage)
+            expect(Response).to receive(:new).and_return(response)
+            expect(response).to receive(:save).and_return(true)
 
             post :create, params: {passage_id: passage.id, user_id: intern.id, text: 'Response to test passage'}
             expect(response).to redirect_to(passages_path)
             expect(flash[:success]).to match('Your response was successfully recorded.')
           end
         end
+
         context 'Response length is invalid' do
           it 'should not create  response' do
-            stub_logged_in(true)
-            user = double('User', admin: false, id: 1)
-            stub_current_user(user)
             expect(ResponsesTracking).to receive(:remaining_time).and_return(2)
+            expect(Passage).to receive(:find).and_return(passage)
+            expect(Passage).to receive(:find).and_return(passage)
+            expect(Response).to receive(:new).and_return(response)
+            expect(response).to receive(:save).and_return(false)
 
             post :create, params: {passage_id: passage.id, user_id: intern.id, text: 'This is an invalid response because its contains more that 140 characters. This is an invalid response because its contains more that 140 chars.'}
             expect(response).to redirect_to(new_passage_response_path(passage))
