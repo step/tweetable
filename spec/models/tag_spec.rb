9b992fc80816dad56f9bd54dd969ca1eb90c79c1
Sooraj Parameswaran <soorajp@thoughtworks.com>
2017-06-02 20:22:55 +0530

    [noCard] [Sooraj] Adding validations for tags
    - Tag name length should not be more than 15

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,7 @@
 describe Tag, type: :model do
-  it do
-    should validate_uniqueness_of(:name)
-  end
+
+  it {should validate_uniqueness_of(:name)}
+
+  it {should validate_length_of(:name).is_at_most(15).on(:create).with_long_message("Tag name length can't be more than 15 characters")}
+
 end
