0a869990ccc9fd0f65a11862b429d85998e26b8b
surajit-chongder <surajit.chongder@gmail.com>
2017-06-03 11:49:47 +0530

    #48 [Surajit/Nimesh] added migration for active user

---
+++ db/migrate/20170602072348_add_active_coloum_to_users.rb
@@ -0,0 +1,5 @@
+class AddActiveColoumToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :active, :boolean, :default => false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602092737) do
+ActiveRecord::Schema.define(version: 20170602072348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20170602092737) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.boolean "active", default: false
   end
 
   add_foreign_key "responses_trackings", "passages"
