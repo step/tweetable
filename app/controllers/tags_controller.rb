22a848d07525c00fd23b82d5d6c694844aa00158
Sooraj Parameswaran <soorajp@thoughtworks.com>
2017-06-07 21:16:34 +0530

    [Sooraj] Fixing redirect url on tag creation
    (cherry picked from commit 2c2e74b35d575b48014e922bb910dc403d53071a)

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,71 +6,55 @@ class TagsController < ApplicationController
   before_action :set_tag, only: [:show, :edit, :update, :destroy]
   layout false, only: [:new, :edit]
 
-  # GET /tags
-  # GET /tags.json
   def index
     @tags = Tag.all.order('name ASC')
   end
 
-  # GET /tags/1
-  # GET /tags/1.json
   def show
   end
 
-  # GET /tags/new
   def new
     @tag = Tag.new
   end
 
-  # GET /tags/1/edit
   def edit
   end
 
-  # POST /tags
-  # POST /tags.json
   def create
     @tag = Tag.new(tag_params)
-
-    respond_to do |format|
-      if @tag.save
-        format.html { redirect_to @tag, notice: 'Tag was successfully created.' }
-        format.json { render :show, status: :created, location: @tag }
-      else
-        format.html { render :new }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
-      end
+    if @tag.save
+      flash[:success] = 'Tag was successfully created.'
+    else
+      flash[:danger] = 'Tag was cannot be created.'
     end
+    redirect_to tags_url
   end
 
-  # PATCH/PUT /tags/1
-  # PATCH/PUT /tags/1.json
   def update
     @tag.update(tag_params)
   end
 
-  # DELETE /tags/1
-  # DELETE /tags/1.json
   def destroy
     @tag.destroy
     respond_to do |format|
-      format.html { redirect_to tags_url, notice: 'Tag was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html {redirect_to tags_url, notice: 'Tag was successfully destroyed.'}
+      format.json {head :no_content}
     end
   end
 
-
   def xeditable? object = nil
-    true # Or something like current_user.xeditable?
+    true
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_tag
-      @tag = Tag.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_tag
+    @tag = Tag.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def tag_params
-      params.require(:tag).permit(:name, :weight, :description)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def tag_params
+    params.require(:tag).permit(:name, :weight, :description)
+  end
+
 end
