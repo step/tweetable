36b7a9cff695dffa772b5c4a85f64999613581c2
Abhishek Gupta <abhishkg@thoughtworks.com>
2017-06-19 13:09:40 +0530

    [Abhishek] Fixing the lints For the leader board extracted the methods and fixed the lints.

--- app/models/leader_board.rb
+++ app/models/leader_board.rb
@@ -1,25 +1,36 @@
 # frozen_string_literal: true
 
+# This class Manages the Leader Board
 class LeaderBoard
-
-  def self.generate_list(users)
-    users_score = users.map do |user| {
-      image: user.image_url,
-        name: user.name,
-        points: user.tags.reduce(0) { |score, tag| score + tag.weight.to_i } }
-    end
-    users_score = users_score.sort_by { |user| user[:points] }.reverse
+  class << self
+  def generate_list(users)
+    users_score = calculate_score(users).sort_by { |user| user[:score] }.reverse
     users_score.first[:rank] = 1
     users_score.each_with_index do |user, index|
-      unless index.eql? 0
-        competitor = users_score[index - 1]
-        if user[:points].eql? competitor[:points]
-          user[:rank] = competitor[:rank]
-        else
-          user[:rank] = competitor[:rank].next
-        end
-      end
+      next if index.eql? 0
+      competitor = users_score[index.pred]
+      user[:rank] = assign_rank(user, competitor)
     end
   end
 
+  def calculate_score(users)
+    users.map do |user|
+      {
+        image: user.image_url,
+        name: user.name,
+        score: user.tags.reduce(0) { |score, tag| score + tag.weight.to_i }
+      }
+    end
+  end
+
+    private
+
+  def equal_points?(user, competitor)
+    user[:score].eql? competitor[:score]
+  end
+
+  def assign_rank(user, competitor)
+    equal_points?(user, competitor) ? competitor[:rank] : competitor[:rank].next
+  end
+end
 end
--- spec/models/leader_board_spec.rb
+++ spec/models/leader_board_spec.rb
@@ -1,24 +1,38 @@
 # frozen_string_literal: true
 
 describe LeaderBoard, type: :model do
-  context 'leader board is calculated on the basis of the credit points they got' do
+  context 'aggreagate the score of the users based on the weight' do
+    it 'should add the score of a user and assign it as score' do
+      tag1 = double('tag', weight: 5)
+      tag2 = double('tag', weight: 10)
+      user1 = double('user', name: 'user1', image_url: '/i', tags: [tag1, tag2])
+      user2 = double('user', name: 'user2', image_url: '/i', tags: [tag1])
+
+      expected = [{ image: '/i', name: 'user1', score: 15 },
+                  { image: '/i', name: 'user2', score: 5 }]
+      user_score = LeaderBoard.calculate_score([user1, user2])
+
+      expect(user_score).to match_array(expected)
+    end
+  end
+
+  context 'leader board is calculated on the basis of the points they got' do
     it 'should calculate rank of the user' do
       tag1 = double('tag', weight: 5)
       tag2 = double('tag', weight: 10)
-      user1 = double('user', name: 'user1', image_url: 'url', tags: [tag1, tag2])
-      user2 = double('user', name: 'user2', image_url: 'url', tags: [tag1])
-      user3 = double('user', name: 'user3', image_url: 'url', tags: [tag2])
-      user4 = double('user', name: 'user3', image_url: 'url', tags: [])
-      user5 = double('user', name: 'user3', image_url: 'url', tags: [tag1])
+      user1 = double('user', name: 'user1', image_url: '/i', tags: [tag1, tag2])
+      user2 = double('user', name: 'user2', image_url: '/i', tags: [tag1])
+      user3 = double('user', name: 'user3', image_url: '/i', tags: [tag2])
+      user4 = double('user', name: 'user4', image_url: '/i', tags: [])
+      user5 = double('user', name: 'user5', image_url: '/i', tags: [tag1])
 
-
-      expected = [{ image: 'url', name: 'user1', points: 15, rank: 1 },
-                  { image: 'url', name: 'user3', points: 10, rank: 2 },
-                  { image: 'url', name: 'user3', points: 5, rank: 3 },
-                  { image: 'url', name: 'user2', points: 5, rank: 3 },
-                  { image: 'url', name: 'user3', points: 0, rank: 4 }]
-
-      leader_board = LeaderBoard.generate_list(users = [user1, user2, user3, user4, user5])
+      expected = [{ image: '/i', name: 'user1', score: 15, rank: 1 },
+                  { image: '/i', name: 'user3', score: 10, rank: 2 },
+                  { image: '/i', name: 'user5', score: 5, rank: 3 },
+                  { image: '/i', name: 'user2', score: 5, rank: 3 },
+                  { image: '/i', name: 'user4', score: 0, rank: 4 }]
+      users = [user1, user2, user3, user4, user5]
+      leader_board = LeaderBoard.generate_list(users)
 
       expect(leader_board).to match_array(expected)
     end
