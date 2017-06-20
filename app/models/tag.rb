cb0285289620ebfc4df7bcda13ab5a860c5cfa34
Sooraj Parameswaran <soorajp@thoughtworks.com>
2017-06-20 17:12:01 +0530

    [Surajit] [tweetable/#75] Confirmation on Tag Deletion - dependent deletion of tagging on deletion of tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,5 +4,5 @@ class Tag < ApplicationRecord
   validates_uniqueness_of :name
   validates_length_of :name, maximum: 25, on: :create, too_long: "Tag name length can't be more than 25 characters"
 
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
 end
