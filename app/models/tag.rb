8451c71d5576c0f1ebb99c07de33b49f1ac02ba0
dharmenn <dharmenn@thoughtworks.com>
2017-06-20 17:43:59 +0530

    [Dharmenn] [tweetable/#76] Add color to a tag :
     - Adding validation for tag color

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,10 @@
 class Tag < ApplicationRecord
   validates_uniqueness_of :name
   validates_length_of :name, maximum: 25, on: :create, too_long: "Tag name length can't be more than 25 characters"
+  validates :color, format: {
+    with: /\A#+([a-fA-F0-9]{6}|[a-fA-F0-9]{3})\z/,
+    message: proc { |_, attributes| "#{attributes[:value]} is not avalid color code." }
+  }
 
   has_many :taggings, dependent: :destroy
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -4,4 +4,28 @@ describe Tag, type: :model do
   it { should validate_uniqueness_of(:name) }
 
   it { should validate_length_of(:name).is_at_most(25).on(:create).with_long_message("Tag name length can't be more than 25 characters") }
+
+  context 'Tag color validation' do
+    context 'When hexcode length is neither 6 nor 3' do
+      it { should_not allow_value('#A8B4').for(:color) }
+    end
+
+    context 'When hexcode length is 6 or 3' do
+      it 'Should not allow other than hexcode' do
+        should_not allow_value('#c8d0fg').for(:color)
+      end
+
+      it 'Should allow hexcode' do
+        should allow_value('#A8B0FC').for(:color)
+      end
+
+      it 'Should allow hexcode with small alphabets' do
+        should allow_value('#a8b0fc').for(:color)
+      end
+
+      it 'Should allow hexcode with length 3' do
+        should allow_value('#A8B').for(:color)
+      end
+    end
+  end
 end
