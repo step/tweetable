b7185f88ded65e3ae29576b8140d8560b8dba558
Sooraj Parameswaran <soorajp@thoughtworks.com>
2017-06-24 13:14:06 +0530

    [Sooraj] Tags names are unique case insensitively

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Tag < ApplicationRecord
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, case_sensitive: false
   validates_length_of :name, maximum: 25, on: :create, too_long: "Tag name length can't be more than 25 characters"
   validates :color, format: {
     with: /\A#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})\Z/,
