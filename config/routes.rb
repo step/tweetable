54e3dd2860d7bbd15e7d788d3cbce1c65bcbc0e9
Vathsala-M <vathsala@thoughtworks.com>
2017-06-26 19:37:55 +0530

    [Vatsala/Sooraj] Add Custom Error templates for 400 and 500

--- config/routes.rb
+++ config/routes.rb
@@ -46,4 +46,7 @@ Rails.application.routes.draw do
   end
 
   resources :tags, except: [:edit, :show]
+
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 end
---
+++ spec/controllers/errors_controller_spec.rb
@@ -0,0 +1,20 @@
+describe ErrorsController do
+  before(:each) do
+    stub_logged_in(true)
+    stub_current_active_admin_user
+  end
+
+  describe "GET #not_found" do
+    it "returns http success" do
+      get :not_found
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe "GET #internal_server_error" do
+    it "returns http success" do
+      get :internal_server_error
+      expect(response).to have_http_status(:internal_server_error)
+    end
+  end
+end
