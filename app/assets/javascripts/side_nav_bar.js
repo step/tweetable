f7770563d6a06352d55446e5df0a6c553ed72f8a
dharmenn <dharmenn@thoughtworks.com>
2017-07-03 23:27:46 +0530

    [Dharmenn] Setting side-nav-bar height on small devices

--- app/assets/javascripts/side_nav_bar.js
+++ app/assets/javascripts/side_nav_bar.js
@@ -1,4 +1,19 @@
 document.addEventListener("turbolinks:load", function () {
   var activeTab = $('.app-main-body > div:first-child').data('tab-name');
   $('.app-navbar-collapse li[data-tab-name='+activeTab+']').addClass('app-active-tab');
+
 });
+function fixSideNavBarHeight(e) {
+  var sideNavBAr = $($(e).data('target'));
+
+  let attr = sideNavBAr.attr('aria-expanded') || 'false';
+  if(attr.match(/^true$/i)) {
+    sideNavBAr.hide();
+    $('.app-sidebar-links-container').css('height', 'auto');
+  }
+  else {
+    sideNavBAr.show();
+    $('.app-sidebar-links-container').css('height', '100%');
+  }
+
+}
